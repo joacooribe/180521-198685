27520d496409ab667eef296cfc4c030dc91d1dc0
DiegoBalbi <diegobalbi1993@gmail.com>
2017-05-07 14:30:26 -0300

    Colabortor Same Email [GREEN]

--- Test/Domain/ColaboratorTest.cs
+++ Test/Domain/ColaboratorTest.cs
@@ -73,5 +73,25 @@ namespace Test.Domain
 
             Assert.IsTrue(colaborator1.Equals(colaborator2));
         }
+
+        [TestMethod]
+        public void ColaboratorDifferentEmail()
+        {
+            colaborator1 = new Colaborator();
+            colaborator1.name = "Diego";
+            colaborator1.surname = "Balbi";
+            colaborator1.mail = "diegobalbi1993@gmail.com";
+            colaborator1.password = "12345";
+            colaborator1.birthday = DateTime.Today;
+
+            colaborator2 = new Colaborator();
+            colaborator2.name = "Joaquin";
+            colaborator2.surname = "Oribe";
+            colaborator2.mail = "joacooribe@gmail.com";
+            colaborator2.password = "1234";
+            colaborator2.birthday = DateTime.Today;
+
+            Assert.IsFalse(colaborator1.Equals(colaborator2));
+        }
     }
 }
