7831f983bf2d2f7562ffc9d9087559ecb292f3a5
DiegoBalbi <diegobalbi1993@gmail.com>
2017-05-15 03:00:41 -0300

    Comment Test [RED]

---
+++ Domain/Comment.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class Comment
+    {
+        public DateTime creationDate { get; set; }
+        public DateTime resolvedDate { get; set; }
+        public User userCreator { get; set; }
+        public User userResolving { get; set; }
+    }
+}
