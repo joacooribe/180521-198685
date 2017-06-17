01fe44bab3fde09d423f9f77c41c8b2f9600bacc
DiegoBalbi <diegobalbi1993@gmail.com>
2017-06-17 12:49:50 -0300

    ElementConnection Clase

---
+++ Domain/Element/ElementConnection/ElementConnection.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Domain
+{
+    public enum Direction { Bidirectional , Directional, None}
+    [Table("Images")]
+    public class ElementConnection : Element
+    {
+        [Key]
+        public int OIDElementConnection { get; set; }
+        public ICollection<Element> Connetions { get; set; }
+        public Direction TypeOfConnection { get; set; }
+    }
+}
